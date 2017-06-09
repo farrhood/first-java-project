1693803972fa5e2b0c79c70f70fc74cd8df03664
Farhood Fazeli <farhood.fazeli@gmail.com>
2017-06-09 16:21:53 +0200

    saving progress exercises

---
+++ src/Ex2a.java
@@ -0,0 +1,8 @@
+public class Ex2a {
+    public static void main(String[] args) {
+        int price = 35;
+        int quantity = 5;
+        double tax = 0.21;
+        System.out.println("The total price is €" + quantity * price * (1 + tax));
+    }
+}
---
+++ src/Ex2bAgePredictor.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ex2bAgePredictor {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Geef uw naam op:");
+
+        String naam = s.next();
+
+        System.out.println("Geef uw leeftijd");
+
+        int age = s.nextInt();
+        
+        System.out.println("Hello " + naam + "! Next year you will be " + (age + 1));
+    }
+}
